4955b62d69c15ea27c1b0a07fb87cd825d4f9367
Hyeonwol <cys8690@naver.com>
2023-11-23 16:07:28 +0900

    상대경로 수정

--- HyeonwolEngine_Window/HyeonLoadResources.h
+++ HyeonwolEngine_Window/HyeonLoadResources.h
@@ -7,13 +7,13 @@ namespace Hyeon
 	void LoadResources()
 	{
 		HyeonResources::Load<graphics::HyeonTexture>(L"BG",
-			L"D:\\GameProgramming\\HyeonwolEngine\\Resources\\Chrono Trigger\\SNES - Chrono Trigger - Guardia Forest.png");
+			L"..\\Resources\\Chrono Trigger\\SNES - Chrono Trigger - Guardia Forest.png");
 
 		HyeonResources::Load<graphics::HyeonTexture>(L"Portal",
-			L"D:\\GameProgramming\\HyeonwolEngine\\Resources\\Chrono Trigger\\Chrono Trigger - Gate.png");
+			L"..\\Resources\\Chrono Trigger\\Chrono Trigger - Gate.png");
 
 		HyeonResources::Load<graphics::HyeonTexture>(L"Title",
-			L"D:\\GameProgramming\\HyeonwolEngine\\Resources\\Chrono Trigger\\Title.png");
+			L"..\\Resources\\Chrono Trigger\\Title.png");
 
 	}
 }
